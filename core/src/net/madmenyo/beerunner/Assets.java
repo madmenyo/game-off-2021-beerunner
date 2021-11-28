ab318c39569c70303292b7e781282a2f80876e30
madmenyo <m.gouw@live.com>
2021-11-28 21:17:22 +0100

    Added models used in previous commit and fixed Assets to work with GWT. However, GWT still does not compile.

--- core/src/net/madmenyo/beerunner/Assets.java
+++ core/src/net/madmenyo/beerunner/Assets.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Cubemap;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.loader.G3dModelLoader;
+import com.badlogic.gdx.utils.GdxRuntimeException;
 import com.badlogic.gdx.utils.JsonReader;
 
 import java.util.ArrayList;
@@ -28,29 +29,42 @@ public class Assets {
 
         for (FileHandle file : Gdx.files.internal("models/trees").list()){
 
-            if (file.file().getName().endsWith("png")) continue;
+            //if (file.file().getName().endsWith("png")) continue;
 
-            AssetDescriptor<Model> treeDescriptor = new AssetDescriptor<>(file, Model.class);
-            assetManager.load(treeDescriptor);
-            trees.add(treeDescriptor);
+            try {
+
+                AssetDescriptor<Model> treeDescriptor = new AssetDescriptor<>(file, Model.class);
+                assetManager.load(treeDescriptor);
+                trees.add(treeDescriptor);
+            } catch (GdxRuntimeException e){
+                System.out.println(e.getMessage());
+            }
         }
 
         for (FileHandle file : Gdx.files.internal("models/rocks").list()){
 
-            if (file.file().getName().endsWith("png")) continue;
+            //if (file.file().getName().endsWith("png")) continue;
 
-            AssetDescriptor<Model> rockDescriptor = new AssetDescriptor<>(file, Model.class);
-            assetManager.load(rockDescriptor);
-            rocks.add(rockDescriptor);
+            try {
+
+                AssetDescriptor<Model> rockDescriptor = new AssetDescriptor<>(file, Model.class);
+                assetManager.load(rockDescriptor);
+                rocks.add(rockDescriptor);
+            } catch (GdxRuntimeException e){
+                System.out.println(e.getMessage());
+            }
         }
 
         for (FileHandle file : Gdx.files.internal("models/flowers").list()){
 
-            if (file.file().getName().endsWith("png")) continue;
-
-            AssetDescriptor<Model> rockDescriptor = new AssetDescriptor<>(file, Model.class);
-            assetManager.load(rockDescriptor);
-            flowers.add(rockDescriptor);
+            //if (file.file().getName().endsWith("png")) continue;
+            try {
+                AssetDescriptor<Model> rockDescriptor = new AssetDescriptor<>(file, Model.class);
+                assetManager.load(rockDescriptor);
+                flowers.add(rockDescriptor);
+            } catch (GdxRuntimeException e){
+                System.out.println(e.getMessage());
+            }
         }
 
 
